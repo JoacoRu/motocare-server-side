a474efdb52452155757576864b718f1954115cc2
Alejandro Walo <walo.walo@gmail.com>
2019-10-05 19:14:54 -0300

    refactor

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,15 @@ const config = require('../config').jwt;
 
 module.exports = function (req, res, next) {
    const token = req.header('x-auth-token');
-   if (!token) return res.status(401).send('Acceso denegado. Clave incorrecta.');
+   if (!token) return res.status(401).send('');
 
    try {
        const decoded = jwt.verify(token, config.privateKey);
+       if(!decoded) return res.status(401).send('');
        req.user = decoded;
        next();
    }
    catch(ex) {
-       res.status(400).send('Clave incorrecta')
+       res.status(401).send('')
    }
 }
